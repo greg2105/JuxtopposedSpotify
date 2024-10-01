cdf17dfcd67ad1025c19ac8c2252d8c747adf876
greg2105 <kinggreg2105@gmail.com>
2024-09-30 20:05:08 -0400

    added some of the skeleton for home layout

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,18 @@
+import notifications from '../assets/notifications.svg';
+import privateicon from '../assets/privateicon.svg';
+import friends from '../assets/friends.svg';
+import settings from '../assets/settings.svg';
+
 const Nav = () => {
     return (
         <div className="Nav">
-            Nav
+            <div className="navRightIcons"> 
+                <button className="notificationsButton"><img src={notifications} alt="" /></button> 
+                <button className="privateButton"><img src={privateicon} alt="" /></button> 
+                <button className="friendsButton"><img src={friends} alt="" /></button>
+                <button className="settingsButton"><img src={settings} alt="" /></button>
+                <button className="profileButton"><img src={settings} alt="" /></button>
+            </div>
         </div>
     )
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,15 +1,34 @@
+import pinsIcon from '../assets/pin.svg';
+import playlistsIcon from '../assets/playlist.svg';
+import likedsongsIcon from '../assets/like.svg';
+import savesIcon from '../assets/saved.svg';
+import albumsIcon from '../assets/album.svg';
+import foldersIcon from '../assets/folder.svg';
+import podcastsIcon from '../assets/podcast.svg';
+import audiobooksIcon from '../assets/audiobook.svg';
+import artistsIcon from '../assets/artist.svg';
+
 const Sidebar = () => {
     return (
         <div className="Sidebar">
-            <button>pins</button>
-            <button>playlists</button>
-            <button>likedsongs</button>
-            <button>saves</button>
-            <button>albums</button>
-            <button>folders</button>
-            <button>podcasts</button>
-            <button>audiobooks</button>
-            <button>artists</button>
+            <button>
+                <img src={pinsIcon} className="pins" /> pins </button>
+            <button>
+                <img src={playlistsIcon} className="playlists" />playlists</button>
+            <button>
+                <img src={likedsongsIcon} className="likedsongs" />likedsongs</button>
+            <button>
+                <img src={savesIcon} className="saves" />saves</button>
+            <button>
+                <img src={albumsIcon} className="albums" />albums</button>
+            <button>
+                <img src={foldersIcon} className="folders" />folders</button>
+            <button>
+                <img src={podcastsIcon} className="podcasts" />podcasts</button>
+            <button>
+                <img src={audiobooksIcon} className="audiobooks" />audiobooks</button>
+            <button>
+                <img src={artistsIcon} className="artists" />artists</button>
         </div>
     ) 
 }
